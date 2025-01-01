2adea4ef9536afd5785d3c03d128308bce83d8eb
ashik <ashik.moinul150@gmail.com>
2025-01-01 22:57:52 +0600

    Add basic navigation & navigator

---
+++ lib/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_essentials/profile.dart';
+import 'package:flutter_essentials/settings.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink.shade50,
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Settings();
+                    },
+                  ),
+                );
+              },
+              child: Text('Go to Settings'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Profile(userName: 'Imran',),
+                    ),
+                  ).then((value) {
+                    print(value);
+                  });
+                },
+                child: Text('Go to Profile')),
+          ],
+        ),
+      ),
+    );
+  }
+}
